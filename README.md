41bc30838bcbb2dfaf21c0c05002607fd96a33b1
Peter Stevens <peterstevens510@gmail.com>
2021-07-08 20:06:54 +0100

    Project now finished, if I can think of any changes I will update later

--- README.md
+++ README.md
@@ -1,35 +1,54 @@
 # Timer App - React
 
-<!-- To add GIF Image -->
+<p align="center">
+  <img src="./src/assets/DemoOfApp.gif" alt="animated" />
+</p>
 
-<!-- <p align="center">
-  <img src="NewDemoOfApp.gif" alt="animated" />
-</p> -->
+# The Project
 
-A simple timer application made with React. With this application you can set a timer in minutes and once started the timer will count down in real time until it reaches 0. Once started, you have the option to pause and resume the timer as well as resetting it. Once the timer hits 00:00:00, an alarm sound will play to notify you that your time is up!
+A simple timer application made with React. With this application you can set a timer in minutes and once you click start the timer will count down in real time until it reaches 0. Once started, you have the option to pause and resume the timer as well as resetting it. Once the timer hits 00:00:00, an alarm sound will be triggered to notify you that your time is up!
 
-# Visit the deployed app
+# User Stories
 
-You can find the deployed app on Heroku via the following link:
-https://my-timer-application.herokuapp.com/
+```
+As a user
+So I can set a timer 
+I'd like to able enter a time limit and be able to set it. 
+```
+
+```
+As a user 
+so I can momentarily stop the timer from running
+I'd like to have pause and resume buttons.
+```
+
+```
+As a user 
+so I know when my time is up
+I'd like to have an alarm sound notification.
+```
  
 # Installation
 
-1. Clone the repo
+1. Clone this repository
 
-2. type "git clone https://github.com/Thatguy560/Timer-React" in the terminal
+2. type git clone "https://github.com/Thatguy560/Timer-React" in the terminal
 
 3. cd into 'Timer-React' (Change directory's)
 
-4. npm install
+4. Install [node.js](https://nodejs.org/en/download/) and run npm install in command line to install dependencies
 
-# Using The App Locally
+# How To Run (Locally)
 
 1. Open up a terminal in your text editor and type 'npm start'
 
 2. This should then automatically open the app on your browser, however if it doesn't just open up any browser of your choosing and type in the url "http://localhost:3000/".
 
-3. Once the page is loaded, type in how long you want to set a timer for (In minutes) and click "Set Timer". Once you're ready click "Start Timer". You also have the option to pause or resume the timer or reset it.
+3. Once the page is loaded, type in how long you want to set a timer for (In minutes) and click "Set Timer". Once you're ready click "Start Timer". You also have the option to Pause or Resume the timer or Reset it.
+
+# Alternatively Visit the deployed app
+
+Click [here](https://my-timer-application.herokuapp.com/) for the deployed app on Heroku. 
 
 # Technologies Used
 
--- src/containers/Timer.js
+++ src/containers/Timer.js
@@ -5,14 +5,12 @@ import "../css/App.css";
 
 export function Timer() {
   const [time, setTime] = React.useState(0);
-  const [originalTime, trackStartingTime] = React.useState(0);
   const [timerOn, IsTimerOn] = React.useState(false);
   const trackTime = useRef(0);
 
   const TimerValue = (givenTime) => {
     const timeInputInMinutes = givenTime.target.value * 60;
     setTime(timeInputInMinutes);
-    trackStartingTime(timeInputInMinutes);
   };
 
   const SubmitTime = (e) => {
@@ -20,28 +18,50 @@ export function Timer() {
     document.getElementById("Container").style.display = "none";
     document.getElementById("Countdown").style.display = "inline-block";
     document.getElementById("StartTimer").style.display = "inline-block";
+    document.getElementById("RestartTimer").style.display = "inline-block";
   };
 
   const playAlarmSound = () => {
     const alarmAudio = new UIfx(audio, {
-      volume: 0.3,
+      volume: 0.6,
       throttleMs: 100,
     });
     alarmAudio.play();
   };
 
+  const timeRunningLow = () => {
+    if (time <= 60) document.getElementById("Countdown").style.color = "red";
+  };
+
+  const playPause = () => {
+    if (timerOn === false) {
+      IsTimerOn(true);
+      document.getElementById("ResumeTimer").style.display = "none";
+      document.getElementById("PauseTimer").style.display = "inline-block";
+    } else {
+      IsTimerOn(false);
+      document.getElementById("ResumeTimer").style.display = "inline-block";
+      document.getElementById("PauseTimer").style.display = "none";
+    }
+  };
+
+  React.useEffect(() => {
+    if (time === 0 && timerOn === true) {
+      playAlarmSound();
+      document.getElementById("PauseTimer").style.display = "none";
+    }
+  });
+
   React.useEffect(() => {
     let interval = null;
     if (timerOn) {
       interval = setInterval(() => {
-        if (time === 0) {
+        if (time <= 0) {
           setTime(0);
-          playAlarmSound();
         } else {
           trackTime.current = time - 1;
           setTime(trackTime.current);
-          if (time <= 60)
-            document.getElementById("Countdown").style.color = "red";
+          timeRunningLow();
         }
       }, 1000);
     } else {
@@ -55,7 +75,7 @@ export function Timer() {
       <header className="App-header">
         <div id="Container">
           <h2 id="Intro">
-            How long do you want to set a timer for in Minutes?
+            How long do you want to set a timer for in minutes?
           </h2>
           <p id="Warning">Please enter a number greater than 0.</p>
           <input
@@ -65,9 +85,11 @@ export function Timer() {
             onChange={TimerValue} // (givenTime) => setTime(givenTime.target.value)
           />
           <form onSubmit={SubmitTime}>
-            <button type="submit" id="SetTimer">
-              Set Timer
-            </button>
+            {time > 0 && (
+              <button type="submit" id="SetTimer">
+                Set Timer
+              </button>
+            )}
           </form>
         </div>
         <div>
@@ -82,29 +104,32 @@ export function Timer() {
             <button
               id="StartTimer"
               style={{ display: "none" }}
-              onClick={() => IsTimerOn(true)}
+              onClick={playPause}
             >
               Start Timer
             </button>
           )}
-          {timerOn && time !== 0 && (
-            <button id="PauseTimer" onClick={() => IsTimerOn(false)}>
-              Pause
-            </button>
-          )}
-          {!timerOn && time !== originalTime && time > 0 && (
-            <button id="ResumeTimer" onClick={() => IsTimerOn(true)}>
-              Resume
-            </button>
-          )}
-          {timerOn && (
-            <button
-              id="RestartTimer"
-              onClick={() => window.location.reload(false)}
-            >
-              Reset
-            </button>
-          )}
+          <button
+            id="PauseTimer"
+            style={{ display: "none" }}
+            onClick={playPause}
+          >
+            Pause
+          </button>
+          <button
+            id="ResumeTimer"
+            style={{ display: "none" }}
+            onClick={playPause}
+          >
+            Resume
+          </button>
+          <button
+            id="RestartTimer"
+            style={{ display: "none" }}
+            onClick={() => window.location.reload(false)}
+          >
+            Reset
+          </button>
         </div>
       </header>
     </div>
